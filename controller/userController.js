eb9181aa533560bd1edad2ac82799896c12fb443
imen-mekni <mekni7058@gmail.com>
2026-05-07 20:12:32 +0100

    jwt

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,10 @@
 const { where } = require("sequelize");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+// ("token :  carte d'indintité  ");
 const createAccount = async (req, res) => {
   try {
     const verifuser = await User.findOne({ where: { email: req.body.email } });
@@ -8,12 +12,34 @@ const createAccount = async (req, res) => {
       return res.status(400).send("email already taken");
     }
 
-    const newuser = await User.create(req.body);
-    res.status(201).send("user created");
+    const password = await bcrypt.hash(req.body.password, 10);
+
+    const newuser = await User.create({
+      email: req.body.email,
+      password: password,
+    });
+
+    // token elle contient 3 parties :
+    //  header : infos aala token algorithm
+    // payload : infos aala luser
+    // signature numérique : génèrer par le clé secrete pour la sécurité de token
+
+    const token = jwt.sign(
+      { id: newuser.id, email: newuser.email },
+
+      process.env.JWT_SECRET,
+    );
+
+    res.status(201).json({
+      email: newuser.email,
+      token: token,
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
+
+// json web token
 
 const loginAccount = async (req, res) => {
   try {
@@ -23,7 +49,12 @@ const loginAccount = async (req, res) => {
       return res.status(404).send("account doesnt exist");
     }
 
-    if (verifuser.password === req.body.password) {
+    let passwordCorrect = await bcrypt.compare(
+      req.body.password,
+      verifuser.password,
+    );
+
+    if (passwordCorrect) {
       return res.status(201).send("welcome to ur account");
     } else {
       return res.status(404).send("password uncorrect");
